8a111a0d883b3ed14597aa528c5989c9eb032407
HPGurre <HPGurre@gmail.com>
2013-08-31 00:51:10 +0200

    Initial GS algorithm

---
+++ SAD1/homework1/GS.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+#Data structures
+men = [1,3,5]
+women = [2,4,6]
+isNotWedlocked = {x: True for x in women}
+menPreferenceList = {1: deque([6,4,2]), 3: deque([2,6,4]), 5: deque([6,4,2])}
+#womenPreferenceList = {2: [3,5,1], 4: [5,1,3], 6: [1,5,3]}
+# current
+
+#Open the file and load contents into memory
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        if line.startswith('#'):
+            pass
+        elif line.startswith('n'):
+            n = line[2]
+#         elif 1+1:
+#             print(line, end='')
+
+#Gale-Shapley Algorithm
+while men:
+    man = men.pop()
+    woman = menPreferenceList[man].popleft()
+    if isNotWedlocked[woman]:
+        pass
+#         assign m and w to be engaged
+#     elif (w prefers m to her fiance m')
+#         assign m and w to be engaged, and m' to be free
+#     else
+#         w rejects m
+
+#Print results during or after algorithm?
