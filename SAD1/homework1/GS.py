dc45ac0bf5c18d94445ea19a307eacd96d1cd941
HPGurre <HPGurre@gmail.com>
2013-09-02 22:55:06 +0200

    Update the read of the women preferences, but might need to be tuned a bit.

--- SAD1/homework1/GS.py
+++ SAD1/homework1/GS.py
@@ -5,9 +5,9 @@ import re
 #Data structures
 men = []
 women = []
-#menPreferenceList = {'Ross': deque(['Rachel','Phoebe','Monica']), 'Chandler': deque(['Monica','Rachel','Phoebe']), 'Joey': deque(['Rachel','Phoebe','Monica'])}
 menPreferenceList = {}
-ranking = {'2': {'3':0,'5':1,'1':2}, '4': {'5':0,'1':1,'3':2}, '6': {'1':0,'5':1,'3':2}}
+#ranking = {'2': {'3':0,'5':1,'1':2}, '4': {'5':0,'1':1,'3':2}, '6': {'1':0,'5':1,'3':2}}
+ranking = {}
     
 #REGEX for matching the lines in the file
 manPattern = re.compile("^\d*[13579][ ]")
@@ -18,10 +18,10 @@ womanPrefPattern = re.compile("^\d*[02468][:]")
 #Open the file and load contents into memory
 with open(sys.argv[1], 'r') as f:
     for line in f:
-#         elif line.startswith('n'):
-#             n = line[2]
-#             continue
-        if manPattern.match(line):
+        if line.startswith('n'):
+            n = int(line[2])
+            continue
+        elif manPattern.match(line):
             men.append(line.split()[0])
             continue
         elif womanPattern.match(line):
@@ -31,7 +31,9 @@ with open(sys.argv[1], 'r') as f:
             menPreferenceList[line[0]] = deque(line.split()[1:])
             continue
         elif womanPrefPattern.match(line):
-            print("FIXME: WOMAN PREFERENCES MATCH")
+            ranking[line[0]] = {k: v for k,v in zip((line.split()[1:]),range(n)) }
+            #I am unsure on this above line. That is, how to initialize the map correctly.
+            #It seems to do it correctly, but in the wrong order.
             continue
         
 #Gale-Shapley Algorithm
