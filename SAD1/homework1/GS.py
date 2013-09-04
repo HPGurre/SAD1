1235d49898ad81c2572abaf103d1ba1f6fd27e9d
HPGurre <HPGurre@gmail.com>
2013-09-04 20:30:38 +0200

    Updated algorithm after the the found errors.

--- SAD1/homework1/GS.py
+++ SAD1/homework1/GS.py
@@ -1,56 +1,78 @@
 import sys
 from collections import deque
 import re
+import timeit
+import pprint
+
 
 #Data structures
 men = []
 women = []
 menPreferenceList = {}
-#ranking = {'2': {'3':0,'5':1,'1':2}, '4': {'5':0,'1':1,'3':2}, '6': {'1':0,'5':1,'3':2}}
-ranking = {}
+womenRanking = {}
+menNames = {}
+womenNames = {}
+
+
+#Start calculating the time spend in execution algorithm
+start = timeit.default_timer() 
     
 #REGEX for matching the lines in the file
-manPattern = re.compile("^\d*[13579][ ]")
-womanPattern = re.compile("^\d*[02468][ ]")
-manPrefPattern = re.compile("^\d*[13579][:]")
-womanPrefPattern = re.compile("^\d*[02468][:]")
+nPattern = re.compile('^[n]')
+manPattern = re.compile('^\d*[13579][\s]')
+womanPattern = re.compile('^\d*[02468][\s]')
+manPrefPattern = re.compile('^\d*[13579][:]')
+womanPrefPattern = re.compile('^\d*[02468][:]')
 
 #Open the file and load contents into memory
 with open(sys.argv[1], 'r') as f:
-    for line in f:
-        if line.startswith('n'):
-            n = int(line[2])
-            continue
+    for line in f:  
+        if nPattern.match(line):
+            n = int(line[2:])
+            
         elif manPattern.match(line):
-            men.append(line.split()[0])
-            continue
+            tokens = line.split()
+            men.append(tokens[0])
+            menNames[tokens[0]] = tokens[1]   
+                   
         elif womanPattern.match(line):
-            women.append(line.split()[0])
-            continue
+            tokens = line.split()
+            women.append(tokens[0])
+            womenNames[tokens[0]] = tokens[1]
+            
         elif manPrefPattern.match(line):
-            menPreferenceList[line[0]] = deque(line.split()[1:])
-            continue
+            tokens = line.split()
+            menPreferenceList[tokens[0][:-1]] = deque(tokens[1:])
+            
         elif womanPrefPattern.match(line):
-            ranking[line[0]] = {k: v for k,v in zip((line.split()[1:]),range(n)) }
+            tokens = line.split()
+            womenRanking[tokens[0][:-1]] = {k: v for k,v in zip((tokens[1:]),range(n)) }
             #I am unsure on this above line. That is, how to initialize the map correctly.
             #It seems to do it correctly, but in the wrong order.
-            continue
-        
-#Gale-Shapley Algorithm
-current = {x: None for x in women}
+
+#Gale-Shapley Algorithm            
+current = {x: None for x in women}        
 
 while men:
     man = men.pop()
     woman = menPreferenceList[man].popleft()
     if current[woman] == None:
         current[woman] = man
-    elif ranking[woman][man] < ranking[woman][current[woman]]:
+    elif womenRanking[woman][man] < womenRanking[woman][current[woman]]:
         discardedMan = current[woman]
         men.append(discardedMan)
         current[woman] = man
     else:
         men.append(man)
 
+#Calculating the run time for the algorithm       
+stop = timeit.default_timer()
+duration = stop-start
+
 #Print results during or after algorithm?..
 print('Result:')
-print(current)
+pretty_ds = pprint.pformat(current, width=20)
+print(pretty_ds)
+
+print("Runtime")
+print (duration)
