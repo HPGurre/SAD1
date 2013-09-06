0f74c70e06dff9a5bb0b3448e574e2229da7409f
HPGurre <HPGurre@gmail.com>
2013-09-07 01:00:20 +0200

    the algorithm is now running, but is is full of errors.

--- SAD1/homework2/Kruskal.py
+++ SAD1/homework2/Kruskal.py
@@ -1,43 +1,53 @@
 import sys
 import re
 
-class Vertex:
-    def __init__(self, name):
-        self.name = name
+# class Vertex:
+#     def __init__(self, name):
+#         self.name = name
         
 class Edge:
     def __init__(self, fromVertex, toVertex, weight): 
         self.fromVertex = fromVertex
         self.toVertex = toVertex
-        self.weight = weight
-    
-class Record:
-    def __init__(self, vertex, pointer): 
-        self.vertex = vertex
-        self.pointer = pointer
-        # FIXME: Implement this(see page 154)
+        self.weight = weight    
         
 class UnionFind:
     components = []
+    parent = {}
+    size = {}
     def __init__(self):
         pass
     def makeUnionFind(self, vertices):
-        """Creates the Union-find data structure with n singleton sets, where n is the no. of vertices
+        """Creates the Union-find data structure with n singleton sets, 
+        where n is the number of vertices
         """
         for vertex in vertices:
-            self.components.append({Record(vertex, None)})
-        # FIXME: Implement this( see page 154) 
+            self.components.append({vertex})
+            self.parent[vertex] = vertex
+            self.size[vertex] = 1
+
     def find(self, vertex):
         """Returns the name of the set that this vertex currently belongs to
         """
-        pass
-        # FIXME: Implement this( see page 154) 
-    def union(self, component1, compoment2):
-        """Unions the two sets that the vertices belong to
-        """
-        pass
-        # FIXME: Implement this (see page 154) 
+        while vertex != self.parent[vertex]: 
+            vertex = self.parent[vertex]
+        return vertex
 
+    def union(self, vertex, anotherVertex):
+        """Weighted Quick-union 
+        """
+        if self.size[vertex] < self.size[anotherVertex]:
+            self.parent[vertex] = anotherVertex
+            self.size[anotherVertex] += self.size[vertex]
+        else:
+            self.parent[anotherVertex] = vertex
+            self.size[vertex] += self.size[anotherVertex]
+    
+    def connected(self, vertex, anotherVertex):
+        """Are they connected?
+        """
+        return self.find(vertex) == self.find(anotherVertex)
+    
 #Data structures
 vertices=[]
 edges=[]
@@ -51,15 +61,20 @@ with open(sys.argv[1], 'r') as f:
     for line in f:  
         
         if cityPattern.match(line):
-            vertices.append(Vertex(line))
+            vertices.append(line.rstrip('\n').strip(' " '))
             
         elif cityDistancePattern.match(line):
             # TODO: Optimize the extraction of the edges. It feels like too much work is being done.
-            fromVertex = line.split('--')[0];
-            toVertex =  line.split('--')[1].split("[")[0][0:-1]
+            fromVertex = line.split('--')[0].strip(' "')
+            toVertex =  line.split('--')[1].split('[')[0].strip(' "')
             weight = int(line.split('--')[1].split("[")[1][0:-2])
             edges.append(Edge(fromVertex, toVertex, weight))
-            
+ 
+for edge in edges:
+    print(edge.fromVertex)
+    print(edge.toVertex)
+    print(edge.weight)
+print(vertices)            
 
 def Kruskal(vertices, edges):
     #Sort edges weights so that c1 <= c2 =< ... <=cm
@@ -77,12 +92,12 @@ def Kruskal(vertices, edges):
     for edge in edges: 
         # (u, v) = ei
           
-        if (unionFind.find(edge.fromVertex) != unionFind.find(edge.toVertex)):
+        if (not unionFind.connected(edge.toVertex, edge.fromVertex)):
             #T = T UNION ei
             T.add(edge)
             
             #merge the sets containing u and v
-            # unionFind.union(set1, set2)
+            unionFind.union(edge.toVertex, edge.fromVertex)
 
     return sum(edge.weight for edge in T)
 
