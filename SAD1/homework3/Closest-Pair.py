11c303748a36abd1ba80b30e70bf17033afa024e
HPGurre <HPGurre@gmail.com>
2013-09-12 14:54:40 +0200

    update

---
+++ SAD1/homework3/Closest-Pair.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def ClosestPair():
+    pass
+    
+    
+    
+    
+#Open the file and load contents into memory
+with open(sys.argv[1], 'r') as f:
+    for line in f:  
+        pass;
