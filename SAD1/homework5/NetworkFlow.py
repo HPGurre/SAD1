eb5d89809ac361ae0516e74f6af28d7cc0064ca7
HPGurre <HPGurre@gmail.com>
2013-09-29 20:53:12 +0200

    update. Nothing works yet.

--- SAD1/homework5/NetworkFlow.py
+++ SAD1/homework5/NetworkFlow.py
@@ -1,4 +1,5 @@
 import timeit
+from collections import deque
 
 class Vertex:
     def __init__ (self, name):
@@ -8,14 +9,38 @@ class Vertex:
         return "Vertex(%s)"%(self.name) 
     
 class Arc:
-    def __init__(self, fromVertex, toVertex, capacity): 
+    def __init__(self, fromVertex, toVertex, capacity, isForward=True): 
         self.fromVertex = fromVertex
         self.toVertex = toVertex
-        self.capacity = capacity  
+        self.capacity = capacity
+        self.isForward = isForward  
         
     def __str__(self):
-        return "Arc(%s --> %s [%s])"%(self.fromVertex, self.toVertex, self.capacity) 
+        direction = 'forward' if self.isForward else 'backward'
+        return "Arc(%s > %s) [%s][%s]"%(self.fromVertex, self.toVertex, self.capacity,direction ) 
+    
+    
+def augment(f, P):
+    b = max(P, key= lambda x :x.capacity) #fixme insert correct key
+#     for arc in P:
+#         if arc.isForward:
+#             pass # increase f(e) in G by b
+#         else:
+#             pass
+    
+    return f
+    
+def maxflow():
+    pass
 
+
+def bfs(G, s):  
+    queue = deque([s])
+    
+    while len(queue) == 0 :
+        queue.popleft()
+    
+    
 start = timeit.default_timer()
 
 file = 'rail.txt'
@@ -35,15 +60,20 @@ with open(file, 'r') as f:
                 line = f.__next__()
                 tokens = line.split() 
                 a = tokens[0]
-                b =  int(tokens[1])
+                b =  tokens[1]
                 c = int(tokens[2])
                 arcs.append(Arc(a, b, c))
-
 
 for v in vertices:
     print(v)
 for a in arcs:
     print(a)
+P = []
+P.append(Arc("a", "b", 10))
+P.append(Arc("a", "b", 1))
+P.append(Arc("a", "b", 2))
+
+augment("s", P)
 stop = timeit.default_timer()
 total = stop-start
 print("Runtime(total): " + str(total))
